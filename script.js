27d3b6ed0989f07859d2bbbaaf5cac13ec6c0778
24-Muhammad-Ikhsan-X-PPLG-2 <ikhsanm181209@gmail.com>
2026-05-15 17:37:14 +0700

    feat: tambah logo w3 footer putih dan oren

---
+++ script.js
@@ -0,0 +1,9 @@
+const logoW3 = document.getElementById("logo-w3-footer");
+
+logoW3.addEventListener("mouseenter", () => {
+  logoW3.src = "/img/w3schools-oren.png";
+});
+
+logoW3.addEventListener("mouseout", () => {
+  logoW3.src = "/img/w3schools-putih.png";
+});
